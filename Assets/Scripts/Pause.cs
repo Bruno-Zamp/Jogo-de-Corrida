83ed45eb3686705bdbc9d3166df99eee016f66eb
Bruno de Almeida Zampirom <brunozampirom@outlook.com>
2018-10-19 16:18:39 -0300

    Jogo 4.0 - Sistema de Pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,49 @@
+﻿// Bruno de Almeida Zampirom - 16/10/2018
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+    [SerializeField]
+    private GameObject panel;
+ 
+    // Update is called once per frame
+    void Update () {
+        
+	    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            panel.SetActive(!panel.active);
+            pausarEdespausar();
+        }
+	}
+    public void Resume()
+    {
+        pausarEdespausar();
+        panel.SetActive(false);
+    }
+    public void Restart(int i)
+    {
+        pausarEdespausar();
+        SceneManager.LoadSceneAsync(i);
+    }
+    public void Menu(int i)
+    {
+        pausarEdespausar();
+        SceneManager.LoadSceneAsync(i);
+    }
+    public void Sair()
+    {
+        Application.Quit();
+    }
+    private void pausarEdespausar()
+    {
+        if (Time.timeScale.Equals(0f))
+        {
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
--- Assets/Scripts/PlayerScoreScript.cs
+++ Assets/Scripts/PlayerScoreScript.cs
@@ -17,7 +17,7 @@ public class PlayerScoreScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
         if (ranking == null) 
         {
